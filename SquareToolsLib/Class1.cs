915c8e5bb579a6ab51118032f4145e87e2f1bbc1
Dintra <alexdintras@mail.ru>
2019-06-22 23:56:30 +0300

    Добавьте файлы проекта.

---
+++ SquareToolsLib/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareToolsLib
+{
+	public abstract class Figure
+	{
+		abstract public double Square();
+	}
+
+    public class Circle: Figure
+    {
+		public double Radius { set; get; }
+
+		//Конструкторы
+		public Circle()
+		{
+		}
+
+		public Circle(double radiusValue)
+		{
+			if(radiusValue == 0)
+			{
+				throw new ArgumentOutOfRangeException("Ошибка: сингулярный круг");
+			}
+			if (radiusValue < 0.0)
+			{
+				throw new ArgumentOutOfRangeException("Ошибка: отрицательное значение радиуса");
+			}
+			Radius = radiusValue;
+		}
+
+		//Методы
+
+		override public double Square()
+		{
+			return 3.141592 * Math.Pow(Radius, 2);
+		}
+    }
+
+	public class Triangle : Figure
+	{
+		//Стороны треугольника
+		public double FirstEdge { set; get; }
+		public double SecondEdge { set; get; }
+		public double ThirdEdge { set; get; }
+
+		//Задаем точность
+		private const double Epsilon = 0.00001;
+
+		//Конструкторы
+		public Triangle()
+		{
+		}
+
+		public Triangle(double fEdge, double sEdge, double tEdge)
+		{
+			//Проверяем на вырожденность
+			if (fEdge < Epsilon || sEdge < Epsilon || tEdge < Epsilon)
+			{
+				throw new ArgumentOutOfRangeException("Ошибка: вырожденный треугольник");
+			}
+
+			//Проверяем на отрицательные значения длин сторон
+			if (fEdge < 0.0 || sEdge < 0.0 || tEdge < 0.0)
+			{
+				throw new ArgumentOutOfRangeException("Ошибка: получено отрицательное значение длины");
+			}
+
+			//Проверяем на выполнение неравенства треугольника
+			bool exp1 = fEdge < sEdge + tEdge;
+			bool exp2 = sEdge < fEdge + tEdge;
+			bool exp3 = tEdge < sEdge + fEdge;
+
+			if (!exp1 || !exp2 || !exp3)
+			{
+				throw new Exception("Ошибка: треугольник не существует, невыполнение неравенства треугольника");
+			}
+
+			FirstEdge  = fEdge;
+			SecondEdge = sEdge;
+			ThirdEdge  = tEdge;
+		}
+
+		//Методы
+
+		public override double Square()
+		{
+			//Используем формулу Герона
+			double semiPer = (FirstEdge + SecondEdge + ThirdEdge) / 2.0;
+			return Math.Sqrt(semiPer * (semiPer - FirstEdge) * (semiPer - SecondEdge) * (semiPer - ThirdEdge));
+		}
+
+		//Проверка на правильный треугольник
+		public bool IsRight()
+		{
+			return Math.Pow(FirstEdge, 2)  == Math.Pow(SecondEdge, 2) + Math.Pow(ThirdEdge, 2) ||
+				   Math.Pow(ThirdEdge, 2)  == Math.Pow(FirstEdge, 2)  + Math.Pow(SecondEdge, 2) ||
+				   Math.Pow(SecondEdge, 2) == Math.Pow(FirstEdge, 2) + Math.Pow(ThirdEdge, 2);
+		}
+	}
+}
---
+++ SquareToolsTests/UnitTest1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using SquareToolsLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquareToolsTests
+{
+	[TestClass]
+	public class Circle_Square
+	{
+		//Задачем точность
+		static readonly double Epsilon = 0.00001;
+
+		[TestMethod]
+		public void Circle_Square_TestNegative()
+		{
+			double radius = -1.0;
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(radius));
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestZeroRadius()
+		{
+			double radius = 0.0;
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(radius));
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestValue_2()
+		{
+			double radius = 2.0;
+
+			double expected = 12.566368;
+
+			Circle c = new Circle(radius);
+			double actual = c.Square();
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestValue_42_7()
+		{
+			double radius = 42.7;
+
+			double expected = 5728.03327768;
+
+			Circle c = new Circle(radius);
+			double actual = c.Square();
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestValue_0_5()
+		{
+			double radius = 0.5;
+
+			double expected = 0.785398;
+
+			Circle c = new Circle(radius);
+			double actual = c.Square();
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestValue_12_05()
+		{
+			double radius = 12.05;
+
+			double expected = 456.16701238;
+
+			Circle c = new Circle(radius);
+			double actual = c.Square();
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+
+		}
+
+		[TestMethod]
+		public void Circle_Square_TestValue_0_017()
+		{
+			double radius = 0.017;
+
+			double expected = 0.000907920088;
+
+			Circle c = new Circle(radius);
+			double actual = c.Square();
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+
+		}
+	}
+
+	[TestClass]
+	public class Triangle_Square
+	{
+		//Задачем точность
+		static readonly double Epsilon = 0.00001;
+
+		[TestMethod]
+		public void Triangle_Square_TestInequality()
+		{
+			Assert.ThrowsException<Exception>(() => new Triangle(2, 3, 10));
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestNegativeValue()
+		{
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(-2, 3, 4));
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestSingularValue()
+		{
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(0.000001, 0.000002, 0.000003));
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestSetValue1()
+		{
+			double fEdge = 5.3;
+			double sEdge = 3.5;
+			double tEdge = 4.01;
+			double expected = 7.017243;
+
+			Triangle triangle = new Triangle(fEdge, sEdge, tEdge);
+			double actual = triangle.Square();
+
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestSetValue2()
+		{
+			double fEdge = 10;
+			double sEdge = 7;
+			double tEdge = 4;
+			double expected = 10.928746;
+
+			Triangle triangle = new Triangle(fEdge, sEdge, tEdge);
+			double actual = triangle.Square();
+
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestSetValue3()
+		{
+			double fEdge = 0.0001;
+			double sEdge = 0.0002;
+			double tEdge = 0.0003;
+			double expected = 0;
+
+			Triangle triangle = new Triangle(fEdge, sEdge, tEdge);
+			double actual = triangle.Square();
+
+			Assert.IsTrue(Math.Abs(actual - expected) <= Epsilon);
+		}
+
+		[TestMethod]
+		public void Triangle_Square_TestRight()
+		{
+			Triangle triangle1 = new Triangle(3, 4, 5);
+			Triangle triangle2 = new Triangle(5, 12, 13);
+			Triangle triangle3 = new Triangle(3, 5, 5);
+			Triangle triangle4 = new Triangle(24, 143, 145);
+
+			Assert.IsTrue(triangle1.IsRight());
+			Assert.IsTrue(triangle2.IsRight());
+			Assert.IsFalse(triangle3.IsRight());
+			Assert.IsTrue(triangle4.IsRight());
+		}
+	}
+}
